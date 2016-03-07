f79d8d29cc90c6daaab6cbeb300e2188fdc0ec1b
JensNordal <jensnordal@gmail.com>
2016-03-06 23:35:24 -0800

    Commit changes for preview

--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
+    // SCROLLMAGIC CONTROLLER
 
+    jQuery(function ($) {
+    var controller = new ScrollMagic.Controller();
+    var tween = TweenMax.staggerFromTo("#parallax2", 2, {
+        backgroundPosition: "0 0"
+    }, {
+        backgroundPosition: "0 150px"
+    });
+    var scene = new ScrollMagic.Scene({
+        duration: 100
+    })
+    scene.setTween(tween);
+    scene.addTo(controller);
+    scene.addIndicators();
+});
+
+
+
+
+
+
+
+
+/*
+
+// init controller
+	var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
+
+	// build scenes
+	new ScrollMagic.Scene({triggerElement: "#parallax1"})
+					.setTween("#parallax1 > div", {y: "80%", ease: Linear.easeNone})
+					.addIndicators()
+					.addTo(controller);
+
+	new ScrollMagic.Scene({triggerElement: "#parallax2"})
+					.setTween("#parallax2 > div", {y: "80%", ease: Linear.easeNone})
+					.addIndicators()
+					.addTo(controller);
+
+	new ScrollMagic.Scene({triggerElement: "#parallax3"})
+					.setTween("#parallax3 > div", {y: "80%", ease: Linear.easeNone})
+					.addIndicators()
+					.addTo(controller);
+*/
---
+++ js/map.js
@@ -0,0 +1,54 @@
+// Map.js ---> Script to run Leaflet.js map
+
+//Set script to run after document load
+$(document).ready(function() {
+
+
+    //Create map and set home location
+    var map = L.map('map-container').setView([47.608013, -122.335167], 14);
+
+	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+
+
+    //Create map layers and set attributes
+    var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttrib});
+
+    var satLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap"   target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a     href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img  src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+        subdomains: ['otile1','otile2','otile3','otile4']
+});
+
+    var drawLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap"   target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a     href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img  src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+        subdomains: ['otile1','otile2','otile3','otile4']
+});
+
+    var mapLayers = {
+        "Satellite": satLayer,
+        "Map View": drawLayer,
+        "Open Street Maps": osm
+};
+
+    L.control.layers(mapLayers).addTo(map);
+        satLayer.addTo(map);
+
+
+    //Add markers, circle, and polygon to identify key areas on map
+    var marker = L.marker([47.608013, -122.335167]).addTo(map);
+        marker.bindPopup('<b>Hello!</b><br>We are located right here in beautiful downtown Seattle, WA<br><a href="#contact">Contact Us for More Info</a>');
+
+
+	//Listen for clicks on map and return lat/long in popup
+	var popup = L.popup();
+
+	function onMapClick(e) {
+    	popup
+        	.setLatLng(e.latlng)
+        	.setContent("You clicked the map at " + e.latlng.toString())
+        	.openOn(map);
+	}
+
+	map.on('click', onMapClick);
+
+    });
